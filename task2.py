1ba79696a7f24775c2ee69f2b16bd26120534fc8
KrzysiekJa <45745634+KrzysiekJa@users.noreply.github.com>
2019-04-07 00:01:41 +0200

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,61 +1,95 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+import weakref
 
-class bank:
-    def __init__(self, name, money, information_storage):
-        self.name  = ""
-        self.money = 10000000
-        self.information_storage = []
-        
-    def moneyInput(self, change):
-        self.money += change
-    
-    def moneyWithdrawal(self, change):
-        self.money -= change
-    
-    def clientInformation(self, information_stream):
-        self.information_storage.append(information_stream)
 
 class client:
     def __init__(self, name1, name2, money):
-        self.name1  = ""
-        self.name2  = ""
-        self.money  = 1000
-        
+        self.name1 = name1
+        self.name2 = name2
+        self.money = 2000
+
     def moneyInput(self, change):
         self.money += change
+                
+    def moneyWithdrawal(self, change):
+        if self.money - change < 0:
+            print("Sorry. Client have not enough money.")
+            return None
+        self.money -= change
+        
+class bank:
+    def __init__(self, bankName = 'Somewhere in Switzerland', bankMoney = 10000000):
+        self.bankName  = bankName
+        self.bankMoney = bankMoney
+        self.client_list         = []
+        self.information_storage = []
+    
+    def moneyInput(self, change):
+        self.bankMoney += change
+        self.information_storage.append(("Bank have input", change))
     
     def moneyWithdrawal(self, change):
-        self.money -= change
+        self.bankMoney -= change
+        self.information_storage.append(("Bank have withdrawal", change))
+    
+    def addClient(self, name1, name2, amount):
+        self.client_list.append(client(name1, name2, amount))
+        self.information_storage.append((name1, name2, amount))
+        self.moneyInput(amount)
+    
+    def moneyChangeClients(self, indexNumber1, indexNumber2, exchange):
+        client1 = self.client_list[indexNumber1]
+        client2 = self.client_list[indexNumber2]
+        client1.moneyWithdrawal(exchange)
+        client2.moneyInput(exchange)
+        self.information_storage.append((client1.name1, client1.name2, client2.name1, client2.name2, "exchanged", exchange))
+    
+    def addInformation(self, information_stream):
+        self.information_storage.append(information_stream)
+    
+    def delClient(self, name1, name2):
+        for client in self.client_list:
+            if (client.name1 == name1 and client.name2 == name2):
+                index = self.client_list.index(client)
+        del self.client_list[index]
+        self.addInformation((name1, name2, 'is out.'))
 
-clent_list = [client(i,i,2900) for i in range(10)]
 
 
-bank1 = bank("JP Morgan", 200000000, clent_list[:5])
-bank2 = bank("PKO", 100000000, clent_list[5:])
+def main():
+    
+    bank1 = bank("JP Morgan", 2000000000)
+    bank2 = bank("PKO", 1000000000)
+    
+    bank1.addClient('Licciardi', 'Vincenzo', 30000000)
+    
+    bank1.addInformation('Ministry of Finance: You will be called by parlamentary commission.')
+    
+    new_clients_list = ['David', 'Rockeffeler', 180000000, 'Wlad', 'Dracula', 15000000, 'Geogre', 'Soros', 2500000, 'Wladimir', 'Putin', 10000000000]
+    
+    for i in range (0, len(new_clients_list) - 2, 3):
+        bank1.addClient(new_clients_list[i], new_clients_list[i+1], new_clients_list[i+2])
+        print('Added to ', bank1.bankName, ': ',new_clients_list[i], new_clients_list[i+1])
+    
+    new_clients_list = ['Jan', 'Kulczyk', 22000000, 'Urszula', 'Moździerz', 15000, 'Modry', 'Robert', 500, 'Wladimir', 'Putin', 10000000000]
+    
+    for i in range (0, len(new_clients_list) - 2, 3):
+        bank2.addClient(new_clients_list[i], new_clients_list[i+1], new_clients_list[i+2])
+        print('Added to ', bank2.bankName, ': ',new_clients_list[i], new_clients_list[i+1])
+    
+    print("\n", bank1.bankName, ":\n", bank1.bankMoney, "\n", bank2.bankName, ":\n", bank1.bankMoney, "\n")
+    print(bank1.bankName, ":\n", bank1.information_storage, "\n", bank2.bankName, ":\n", bank2.information_storage) 
+    
+    bank2.delClient('Wladimir', 'Putin')
+    print("\n", bank1.bankName, ":\n", bank1.bankMoney, "\n", bank2.bankName, ":\n", bank1.bankMoney, "\n")
+    
+    bank2.moneyChangeClients(1, 2, 500) 
+    bank2.addClient('Filipiak', 'Janusz', 45000000)
+    
+    print("\n", bank1.bankName, ":\n", bank1.bankMoney, "\n", bank2.bankName, ":\n", bank1.bankMoney, "\n")
+    bank1.addInformation('Ministry of Finance: Prima Aprilis.')
+    print("\n",bank1.bankName, ":\n", bank1.information_storage, "\n", bank2.bankName, ":\n", bank2.information_storage)
 
-print(bank1, bank2)
+
+
+if __name__=='__main__':
+    main()
