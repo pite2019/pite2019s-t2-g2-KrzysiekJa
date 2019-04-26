9f654a7353612f7f28dc6d361830dd59bfe67147
KrzysiekJa <45745634+KrzysiekJa@users.noreply.github.com>
2019-04-26 10:08:29 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -5,15 +5,16 @@ import re
         
 class Bank:
     def __init__(self, bank_name = 'Somewhere in Switzerland', bank_money = 10000000):
-        self.bank_name   = bank_name
-        self.bank_money  = bank_money
-        self.__clints_file = “%sfile.txt” % (self.bank_name)
-        file = open(self.clints_file, 'x')
+        self.bank_name  = bank_name
+        self.bank_money = bank_money
+        self.__clients_file = '%sfile.txt' %(self.bank_name)
+        
+        file = open(self.__clients_file, 'x')
         file.close()
-    
+        
     def creAccount(self, name, last_name, amount):
         self.bank_money += amount
-        file   = open(self.__clints_file, 'a')
+        file   = open(self.__clients_file, 'a')
         client = {
           'name': name,
           'last_name': last_name,
@@ -27,11 +28,11 @@ class Bank:
         return client
        
     def delAccount(self, name, last_name):
-        with open(self.__clints_file, 'r') as file:
+        with open(self.__clients_file, 'r') as file:
             lines = file.readlines()
-        file = open(self.__clints_file, 'w')
+        file = open(self.__clients_file, 'w')
         for indx in lines:
-            if (re.search("^{"name": "%s", "last_name": "%s"" % (name, last_name),indx) != None):
+            if (re.search('^{"name": "%s", "last_name": "%s"' %(name, last_name),indx) != None):
                 tmp = json.loads(indx)
                 amount = tmp['money']
                 self.bank_money -= amount
@@ -46,11 +47,11 @@ class Bank:
         
     def moneyDeposit(self, name, last_name, change):
         self.bank_money += change
-        with open(self.__clints_file, 'r+') as file:
+        with open(self.__clients_file, 'r+') as file:
             lines = file.readlines()
             file.seek(0)
             for indx in lines:
-                if (re.search("^{"name": "%s", "last_name": "%s"" % (name, last_name),indx) != None):
+                if (re.search('^{"name": "%s", "last_name": "%s"' % (name, last_name),indx) != None):
                     tmp  = json.loads(indx)
                     tmp['money'] += change
                     tmp2 = json.dumps(tmp)
@@ -68,11 +69,11 @@ class Bank:
     def moneyWithdrawal(self, name, last_name, change):
         self.bank_money -= change
         error = 0
-        with open(self.__clints_file, 'r') as file:
+        with open(self.__clients_file, 'r') as file:
             lines = file.readlines()
-        with open(self.__clints_file, 'w') as file:
+        with open(self.__clients_file, 'w') as file:
             for indx in lines:
-                if (re.search("^{"name": "%s", "last_name": "%s"" % (name, last_name),indx) != None):
+                if (re.search('^{"name": "%s", "last_name": "%s"' % (name, last_name),indx) != None):
                     tmp  = json.loads(indx)
                     tmp['money'] -= change
                     if tmp['money'] < 0:
@@ -103,11 +104,11 @@ class Bank:
     def giveCredit(self, name, last_name, amount):
         if amount > 0.1 * self.bank_money:
             return "Unfortunately, not possible."
-        with open(self.__clints_file, 'r') as file:
+        with open(self.__clients_file, 'r') as file:
             lines = file.readlines()
-        with open(self.__clints_file, 'w') as file:
+        with open(self.__clients_file, 'w') as file:
             for indx in lines:
-                if (re.search("^{"name": "%s", "last_name": "%s"" % (name, last_name),indx) != None):
+                if (re.search('^{"name": "%s", "last_name": "%s"' % (name, last_name),indx) != None):
                     tmp  = json.loads(indx)
                     tmp['credit'] = amount
                     tmp2 = json.dumps(tmp)
@@ -123,13 +124,13 @@ class Bank:
 
 
 
-
 def main():
     
     bank1 = Bank("JP Morgan", 20000000000)
     bank2 = Bank("PKO", 1000000000)
     bank3 = Bank("Transfer Bank", 1500000000)
-        
+    
+    
     new_clients_list = ['Licciardi', 'Vincenzo', 30000000, 'David', 'Rockeffeler', 180000000, 'Wlad', 'Dracula', 15000000, 'Geogre', 'Soros', 2500000, 'Wladimir', 'Putin', 10000000000]
     
     for i in range (0, len(new_clients_list) - 2, 3):
@@ -140,11 +141,17 @@ def main():
     
     for i in range (0, len(new_clients_list) - 2, 3):
         result = bank2.creAccount(new_clients_list[i], new_clients_list[i+1], new_clients_list[i+2])
-        print('Added to ', bank2.bankName, ': ', result)
+        print('Added to ', bank2.bank_name, ': ', result)
     
-    result = bank3.creAccount('Filipiak', 'Janusz', 45000000)
-    print('Added to ', bank3.bankName, ': ', result)
+    new_clients_list = ['Filipiak', 'Janusz', 45000000,]
     
+    for i in range (0, len(new_clients_list) - 2, 3):
+        result = bank2.creAccount(new_clients_list[i], new_clients_list[i+1], new_clients_list[i+2])
+        print('Added to ', bank3.bank_name, ': ', result)
+    
+    
+    print(bank2.bank_name, "gives credit:", bank2.giveCredit("Robert", "Robert", 1500))
+    print(bank2.bank_name, "gives credit:", bank2.giveCredit("Wladimir", "Putin", 2000))
     
     
 
